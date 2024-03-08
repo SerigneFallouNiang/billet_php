4497601a7c90e091db999c19efed6df20444e8e6
SerigneFallouNiang <fallouniang776@gmail.com>
2024-03-08 07:55:44 +0000

    style des billets

--- liste.php
+++ liste.php
@@ -9,15 +9,30 @@
                 }else {
                     while($row=mysqli_fetch_assoc($req)){
                         ?>
-            <div class="container1">
-            <p>Client:<?=$row['prenom']?><?=$row['nom']?></p>     
-            <p>prix billet :<?=$row['prix']?>, De <?=$row['depart']?> ,Vers <?=$row['destination']?></p>       
-            <a href="accueil.php?id=<?=$row['id']?>">Detail</a>
-            </div>
+ <div class="projcard-container">
+    
+    <div class="projcard projcard-blue">
+      <div class="projcard-innerbox">
+        <img class="projcard-img" src="https://picsum.photos/800/600?image=1041" />
+        <div class="projcard-textbox">
+          <div class="projcard-title">Client: <?=$row['prenom']?><?=$row['nom']?></div>
+          <div class="projcard-subtitle">Prix du billet : <?=$row['prix']?></div>
+          <div class="projcard-bar"></div>
+          <div class="projcard-description">Nous sommes ravis de vous accueillir à bord de notre vol de <?=$row['depart']?>  à <?=$row['destination']?>. Votre aventure commence le <?=$row['date_reservation'] ?>, et nous sommes impatients de faire partie de votre voyage. Préparez-vous à vivre une expérience de vol exceptionnelle avec nous.
+
+Bon voyage,</div>
+          <div class="projcard-tagbox">
+          </div>
+          <span class="projcard-tag"><a href="accueil.php?id=<?=$row['id']?>">Detail</a></span>
+        </div>
+      </div>
+    </div>
+    </div>
                         <?php
                     }
                     
                 }
+                include'footer.php';
             ?>
               
 
